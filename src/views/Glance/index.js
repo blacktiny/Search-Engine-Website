76e83e822c54bf3842572449891e7efb40bc476a
blacktiny <blacktiny126@hotmail.com>
2019-04-13 13:35:54 -0700

    made photos tile ui

--- src/views/Glance/index.js
+++ src/views/Glance/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import TopResults from '../../common/components/TopResults'
 import Videos from '../../common/components/Videos'
+import Photos from '../../common/components/Photos'
 
 import './index.scss'
 
@@ -9,11 +10,14 @@ class Glance extends Component {
   render() {
     return (
       <div className='glance'>
-        <div className='tile1'>
+        <div className='tile1 tile-topresults'>
           <TopResults />
         </div>
-        <div className='tile1'>
+        <div className='tile1 tile-videos'>
           <Videos />
+        </div>
+        <div className='tile1 tile-photos'>
+          <Photos />
         </div>
       </div>
     );
