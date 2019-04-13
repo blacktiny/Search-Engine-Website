5599ed2389b67cfa04f45934f3b30707b4f76261
blacktiny <blacktiny126@hotmail.com>
2019-04-13 11:53:35 -0700

    added carousel component and implemented top results ui

--- src/views/Glance/index.js
+++ src/views/Glance/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 
+import TopResults from '../../common/components/TopResults'
+
 import './index.scss'
 
 class Glance extends Component {
   render() {
     return (
       <div className='glance'>
-        
+        <div className='tile1'>
+          <TopResults />
+        </div>
       </div>
     );
   }
